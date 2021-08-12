4605653dfc564bc9a13d39b335c17a2fc516870d
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-08-12 01:08:54 -0700

    ref(js): Remove context usage from pagination component

--- static/app/components/pagination/index.tsx
+++ static/app/components/pagination/index.tsx
@@ -1,8 +1,6 @@
-import {Component, ReactElement} from 'react';
-import {browserHistory} from 'react-router';
+import {browserHistory, withRouter, WithRouterProps} from 'react-router';
 import styled from '@emotion/styled';
 import {Query} from 'history';
-import PropTypes from 'prop-types';
 
 import Button from 'app/components/button';
 import ButtonBar from 'app/components/buttonBar';
@@ -12,80 +10,76 @@ import space from 'app/styles/space';
 import {callIfFunction} from 'app/utils/callIfFunction';
 import parseLinkHeader from 'app/utils/parseLinkHeader';
 
-const defaultProps: DefaultProps = {
-  size: 'small',
-  disabled: false,
-  onCursor: (cursor: string, path: string, query: Query, _direction: number) => {
-    browserHistory.push({
-      pathname: path,
-      query: {...query, cursor},
-    });
-  },
-};
-
-type DefaultProps = {
-  size?: 'zero' | 'xsmall' | 'small';
-  onCursor?: (cursor: string, path: string, query: Query, _direction: number) => void;
-  disabled?: boolean;
-};
-
-type Props = DefaultProps & {
+type Props = WithRouterProps & {
   pageLinks?: string | null;
   to?: string;
   /**
    * The caption must be the PaginationCaption component
    */
-  caption?: ReactElement;
+  caption?: React.ReactElement;
+  size?: 'zero' | 'xsmall' | 'small';
+  onCursor?: (cursor: string, path: string, query: Query, _direction: number) => void;
+  disabled?: boolean;
   className?: string;
 };
 
-class Pagination extends Component<Props> {
-  static contextTypes = {
-    location: PropTypes.object,
-  };
+const defaultOnCursor = (
+  cursor: string,
+  path: string,
+  query: Query,
+  _direction: number
+) =>
+  browserHistory.push({
+    pathname: path,
+    query: {...query, cursor},
+  });
 
-  static defaultProps = defaultProps;
-
-  render() {
-    const {className, onCursor, pageLinks, size, caption, disabled} = this.props;
-    if (!pageLinks) {
-      return null;
-    }
-
-    const location = this.context.location;
-    const path = this.props.to || location.pathname;
-    const query = location.query;
-    const links = parseLinkHeader(pageLinks);
-    const previousDisabled = disabled || links.previous.results === false;
-    const nextDisabled = disabled || links.next.results === false;
-
-    return (
-      <Wrapper className={className}>
-        {caption}
-        <ButtonBar merged>
-          <Button
-            icon={<IconChevron direction="left" size="sm" />}
-            aria-label={t('Previous')}
-            size={size}
-            disabled={previousDisabled}
-            onClick={() => {
-              callIfFunction(onCursor, links.previous.cursor, path, query, -1);
-            }}
-          />
-          <Button
-            icon={<IconChevron direction="right" size="sm" />}
-            aria-label={t('Next')}
-            size={size}
-            disabled={nextDisabled}
-            onClick={() => {
-              callIfFunction(onCursor, links.next.cursor, path, query, 1);
-            }}
-          />
-        </ButtonBar>
-      </Wrapper>
-    );
+const Pagination = ({
+  to,
+  location,
+  className,
+  onCursor = defaultOnCursor,
+  pageLinks,
+  size = 'small',
+  caption,
+  disabled = false,
+}: Props) => {
+  if (!pageLinks) {
+    return null;
   }
-}
+
+  const path = to ?? location.pathname;
+  const query = location.query;
+  const links = parseLinkHeader(pageLinks);
+  const previousDisabled = disabled || links.previous.results === false;
+  const nextDisabled = disabled || links.next.results === false;
+
+  return (
+    <Wrapper className={className}>
+      {caption}
+      <ButtonBar merged>
+        <Button
+          icon={<IconChevron direction="left" size="sm" />}
+          aria-label={t('Previous')}
+          size={size}
+          disabled={previousDisabled}
+          onClick={() => {
+            callIfFunction(onCursor, links.previous.cursor, path, query, -1);
+          }}
+        />
+        <Button
+          icon={<IconChevron direction="right" size="sm" />}
+          aria-label={t('Next')}
+          size={size}
+          disabled={nextDisabled}
+          onClick={() => {
+            callIfFunction(onCursor, links.next.cursor, path, query, 1);
+          }}
+        />
+      </ButtonBar>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled('div')`
   display: flex;
@@ -94,4 +88,4 @@ const Wrapper = styled('div')`
   margin: ${space(3)} 0 0 0;
 `;
 
-export default Pagination;
+export default withRouter(Pagination);
